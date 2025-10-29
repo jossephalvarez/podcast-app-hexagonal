21ffdeab4717502f0bb963f6265c80d01aa3c418
jossephalvarez <josseph.alvarez@melia.com>
2025-10-29 22:20:04 +0100

    chore(init): initialize project with npm, TypeScript and Webpack setup

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.tsx',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+    mode: 'development',
+    devServer: {
+        static: './dist',
+        port: 3000,
+    },
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js', '.css'],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './public/index.html',
+        }),
+    ],
+};
